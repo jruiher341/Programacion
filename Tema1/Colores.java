063a30ea3daf22a6bdcd34edc936baac460139ba
jruiher341 <jruiher341@g.educaand.es>
2025-10-03 10:12:10 +0200

    03/10/2025

---
+++ Tema1/Colores.java
@@ -0,0 +1,30 @@
+public class Colores {
+
+    public static void main(String[] args) {
+        System.out.println("\033[30m Gris \033[0m");
+        System.out.println("\033[31m Rojo \033[0m");
+        System.out.println("\033[32m Verde \033[0m");
+        System.out.println("\033[33m Amarillo \033[0m");
+        System.out.println("\033[34m Azul oscuro \033[0m");
+        System.out.println("\033[35m Magenta \033[0m");
+        System.out.println("\033[36m Azul claro \033[0m");
+        System.out.println("\033[37m Blanco \033[0m");
+
+        System.out.println("\033[40m Gris \033[0m");
+        System.out.println("\033[41m Rojo \033[0m");
+        System.out.println("\033[42m Verde \033[0m");
+        System.out.println("\033[43m Amarillo \033[0m");
+        System.out.println("\033[44m Azul oscuro \033[0m");
+        System.out.println("\033[45m Magenta \033[0m");
+        System.out.println("\033[46m Azul claro \033[0m");
+        System.out.println("\033[47m Blanco \033[0m");
+
+        System.out.println("\033[0m");
+
+       System.out.println("\003[38;5;230;94;202m \u2665 \033[0m");
+       
+       System.out.printf("El valor %c es un carácter y este otro %d es un entero. \n %c \n", 'A', 15);
+       System.out.printf("El siguiente valor es un carácter: %c \n", 'A');
+    }
+    
+}
