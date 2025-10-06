7a0c6ca62b057bd81b0e6492b0063516fda52c2a
jruiher341 <jruiher341@g.educaand.es>
2025-10-06 12:24:33 +0200

    Ejercicios Tema 1

---
+++ Tema1/Ejercicios/Ejercicio2.java
@@ -0,0 +1,7 @@
+public class Ejercicio2 {
+
+    public static void main(String[] args){
+
+        System.out.println("Javier\nMojacar\n123456789");
+    }
+}
---
+++ Tema1/Ejercicios/Ejercicio3.java
@@ -0,0 +1,16 @@
+public class Ejercicio3 {
+
+    public static void main(String[] args){
+
+        System.out.println("computer\tordenador");
+        System.out.println("student\testudiante");
+        System.out.println("cat\tgato");
+        System.out.println("dog\tperro");
+        System.out.println("machine\tmaquina");
+        System.out.println("penguin\tpinguino");
+        System.out.println("light\tluz");
+        System.out.println("green\tverde");
+        System.out.println("book\tlibro");
+        System.out.println("pyramid\tpiramide");
+    }
+}
